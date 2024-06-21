5163fb88b1e235807c0219c1af9292c1a4867fc2
Radonchnk <radoncknk@gmail.com>
2024-06-21 22:08:51 +0100

    Added error checking

--- tools/voiceChanger.py
+++ tools/voiceChanger.py
@@ -7,29 +7,35 @@ from rvc_python.infer import infer_file
 class VoiceChange:
     def __init__(self):
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        print(f"Voice changer is loaded on {'cuda' if torch.cuda.is_available() else 'cpu'}")
 
     def changeVoice(self, audioInput, output):
-        self.convToWav(audioInput)
-        audioInput = audioInput.replace(".mp3",".wav")
-        output = output.replace(".mp3",".wav")
-        result = infer_file(
-            input_path=audioInput,
-            model_path="./model.pth",
-            index_path="",
-            device=self.device,
-            f0method="harvest",
-            f0up_key=0,
-            opt_path=output,
-            index_rate=0.5,
-            filter_radius=3,
-            resample_sr=0,
-            rms_mix_rate=0.25,
-            protect=0.33,
-            version="v2",
-        )
-        self.convToMp3(output)
-        print("Inference completed. Output saved to:", result)
-        gc.collect()
+        try:
+            print("Inference STARTED. From:", audioInput)
+            self.convToWav(audioInput)
+            audioInput = audioInput.replace(".mp3",".wav")
+            output = output.replace(".mp3",".wav")
+            # Calling the model to change
+            result = infer_file(
+                input_path=audioInput,
+                model_path="./model.pth",
+                index_path="",
+                device=self.device,
+                f0method="harvest",
+                f0up_key=0,
+                opt_path=output,
+                index_rate=0.5,
+                filter_radius=3,
+                resample_sr=0,
+                rms_mix_rate=0.25,
+                protect=0.33,
+                version="v2",
+            )
+            self.convToMp3(output)
+            print("Inference completed. Output saved to:", result)
+            gc.collect()
+        except Exception as e:
+            print(e)
 
     def convToWav(self, file):
         audioSegment = AudioSegment.from_mp3(file)
