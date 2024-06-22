f619fafce7c7b125af909360248fb28b39bc0074
luke <lukehjgeorgeson@gmail.com>
2024-06-22 21:08:40 +0100

    custom rvc library added - to be integrated

---
+++ tools/RVCPython/downloadModel.py
@@ -0,0 +1,25 @@
+import os
+import requests
+
+
+def downloadRVCModels(dir):
+    folder = os.path.join(dir, 'base_model')
+
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+    files = {
+        "hubert_base.pt": "https://huggingface.co/Daswer123/RVC_Base/resolve/main/hubert_base.pt",
+        "rmvpe.pt": "https://huggingface.co/Daswer123/RVC_Base/resolve/main/rmvpe.pt",
+        "rmvpe.onnx": "https://huggingface.co/Daswer123/RVC_Base/resolve/main/rmvpe.onnx"
+    }
+
+    for filename, url in files.items():
+        filePath = os.path.join(folder, filename)
+        if not os.path.exists(filePath):
+            print(f"Downloading {filename}...\n"+"-"*len(f"Downloading {filename}..."))
+            r = requests.get(url, stream=True)
+            if r.status_code == 200:
+                with open(filePath, 'wb') as f:
+                    f.write(r.content)
+                print(f"Successfully downloaded {filename}\n")
